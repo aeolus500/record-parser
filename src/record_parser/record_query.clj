876c16a36dfeffb06e77bff6211d1306607d475c
Emmanuel San Miguel <esanmiguel@gmail.com>
2019-05-15 08:55:59 -0500

    Sorts by gender

--- src/record_parser/record_query.clj
+++ src/record_parser/record_query.clj
@@ -1,6 +1,11 @@
 (ns record-parser.record-query)
 
+(defn- sort-gender [records]
+  (let [{female "female" male "male"} (group-by :gender records)]
+    (concat (sort-by :last female) (sort-by :last male))))
+
 (defn sort-records [records sort-type]
   (case sort-type
+    :gender (sort-gender records)
     :last (reverse (sort-by :last records))
     (sort-by sort-type records)))
--- test/record_parser/record_query_test.clj
+++ test/record_parser/record_query_test.clj
@@ -4,12 +4,23 @@
             [clojure.test :refer :all]))
 
 (deftest record-query-test
+  (testing "sorts by gender then last name ascending"
+    (is (= [{:last "Eastwood" :gender "female"}
+            {:last "Zapata" :gender "female"}
+            {:last "Abrams" :gender "male"}
+            {:last "Bach" :gender "male"}]
+           (record-query/sort-records [{:last "Zapata" :gender "female"}
+                                       {:last "Bach" :gender "male"}
+                                       {:last "Eastwood" :gender "female"}
+                                       {:last "Abrams" :gender "male"}
+                                       ] :gender)))
+    )
   (testing "sorts by last name descending"
     (is (= [{:last "Zapata"} {:last "Abrams"}]
            (record-query/sort-records [{:last "Abrams"} {:last "Zapata"}] :last)))
     (is (= [{:last "Zapata"} {:last "Bach"} {:last "Abrams"}]
            (record-query/sort-records [{:last "Zapata"} {:last "Abrams"} {:last "Bach"}] :last))))
-  (testing "sorts by birth date ascending"
+  (testing "sorts by birthdate ascending"
     (is (= [{:birthdate (from-string "1990-01-01")}
             {:birthdate (from-string "1991-01-01")}
             {:birthdate (from-string "1992-01-01")}]
